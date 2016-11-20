0fbe69a15128ad8d7a16b06c8a5f612d4f577b7a
Joel Bond <joeltbond@gmail.com>
2016-11-19 18:03:22 -0800

    intial commit

---
+++ src/cljs/synth/core.cljs
@@ -0,0 +1,48 @@
+(ns synth.core
+  (:require [reagent.core :as r]
+            [synth.synth :as s]
+            [cljsjs.react]))
+
+(def
+  app-state
+  (r/atom {
+    :switch-states {}
+    }))
+
+(def notes [
+  220.00 246.94 261.63 293.66 329.63 349.23 392.00
+  440.00 493.88 523.25 587.33 659.25 698.46 783.99
+  880.00
+  ])
+
+(defn get-switch-state [freq]
+  (get-in @app-state [:switch-states freq] false))
+
+(defn start-note [freq]
+  (s/start-note freq))
+
+(defn stop-note [freq]
+  (s/stop-note freq))
+
+(defn on-button-click [freq]
+  (do
+    (swap! app-state update-in [:switch-states freq] not)
+    (if (get-switch-state freq)
+      (start-note freq)
+      (stop-note freq))))
+
+(defn osc-button [freq]
+  [:button
+    {:on-click #(on-button-click freq)
+      :class (if (get-switch-state freq) "on")}])
+
+(defn main-page []
+  [:div {:class "container"}
+    (for [f notes]
+      [osc-button f])])
+
+(defn mount-root []
+  (r/render [main-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (mount-root))
---
+++ src/cljs/synth/synth.cljs
@@ -0,0 +1,31 @@
+(ns synth.synth)
+
+(def ctx (js/AudioContext.))
+
+(def gain (.createGain ctx))
+(set! (.-value gain.gain) 0.1)
+(.connect gain ctx.destination)
+
+(def vib (.createOscillator ctx))
+(def vib-amount (.createGain ctx))
+(set! (.-type vib) "sine")
+(set! (.-value vib.frequency) 1.3)
+(set! (.-value vib-amount.gain) 2)
+(.connect vib vib-amount)
+(.start vib)
+
+(def oscillators (atom {}))
+
+(defn start-note [freq]
+	(let [osc (.createOscillator ctx)]
+		(set! (.-type osc) "sine")
+		(set! (.-value osc.frequency) freq)
+		(.connect osc gain)
+    (.connect vib-amount osc.frequency)
+    (.start osc)
+		(swap! oscillators assoc freq osc)))
+
+(defn stop-note [freq]
+  (let [osc (@oscillators freq)]
+    (.stop osc)
+    (swap! oscillators dissoc freq)))
